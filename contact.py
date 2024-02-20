c108503e6a973aefaf8a2929e6acbf491b6bde9d
Gabe Morris <gabemorris1231@gmail.com>
2024-02-19 17:20:17 -0700

    finished setting up the data structure

--- contact.py
+++ contact.py
@@ -5,17 +5,13 @@ class MeshBody:
     def __init__(self, points, cells_dict, velocity=np.float64([0, 0, 0])):
         self.points, self.cells_dict, self.velocity = points, cells_dict, velocity
         self.surfaces, self.surface_count = [], np.zeros(self._surface_count(), dtype=np.int32)
+        self.surface_dict = dict()
+        self.elements = []
 
-        self.nodes = [Node(i, p, velocity) for i, p in enumerate(points)]
+        self.nodes = np.array([Node(i, p, velocity) for i, p in enumerate(points)], dtype=object)
 
         self._construct_elements()
-
-    def __contains__(self, item):
-        bool_list = [item == surf for surf in self.surfaces]
-        if any(bool_list):
-            return bool_list.index(True)
-        else:
-            return None
+        self.surface_count = self.surface_count[self.surface_count > 0]
 
     def _surface_count(self):
         element_count = 0
@@ -30,32 +26,46 @@ class MeshBody:
     def _construct_elements(self):
         for key, value in self.cells_dict.items():
             if key == 'hexahedron':
-                elements = [self._construct_hexahedron(row) for row in value]
+                elements = [self._construct_hexahedron(row, key, i) for i, row in enumerate(value)]
+                self.elements.extend(elements)
 
-    def _construct_hexahedron(self, connectivity):
-        face1 = list(connectivity[:4])
-        face2 = list(connectivity[4:])
+    def _construct_hexahedron(self, con, elem_type, label):
+        face1 = self.nodes[con[:4]]
+        face1 = Surface(0, list(face1))
+        face2 = self.nodes[con[4:]]
+        face2 = Surface(0, list(face2))
+        face3 = Surface(0, [face1[0], face2[0], face2[1], face1[1]])
+        face4 = Surface(0, [face1[1], face2[1], face2[2], face1[2]])
+        face5 = Surface(0, [face1[2], face2[2], face2[3], face1[3]])
+        face6 = Surface(0, [face1[3], face2[3], face2[0], face1[0]])
 
-        v1 = self.points[face1[1]] - self.points[face1[0]]
-        v2 = self.points[face1[2]] - self.points[face1[1]]
-        v3 = self.points[face2[0]] - self.points[face1[0]]
-        v4 = self.points[face2[1]] - self.points[face2[0]]
-        v5 = self.points[face2[2]] - self.points[face2[0]]
+        surfs = [face1, face2, face3, face4, face5, face6]
 
-        if np.dot(np.cross(v1, v2), v3) > 0:
-            others = face1[1:]
-            others.reverse()
-            face1 = [face1[0]] + others
+        for i, surf in enumerate(surfs):
+            nodes = tuple(sorted([node.label for node in surf.nodes]))
+            existing_surf = self.surface_dict.get(nodes)
+            if existing_surf is not None:
+                surfs[i] = existing_surf
+                self.surface_count[existing_surf.label] += 1
+            else:
+                n = len(self.surfaces)
+                surf.label = n
+                self.surface_count[n] += 1
+                self.surfaces.append(surf)
+                self.surface_dict.update({nodes: surf})
 
-        if np.dot(np.cross(v4, v5), v3) < 0:
-            others = face2[1:0]
-            others.reverse()
-            face2 = [face2[0]] + others
+        return Element(label, elem_type, self.nodes[con], surfs)
+
+    def __repr__(self):
+        return f'MeshBody(elements={len(self.elements)}, surfaces={len(self.surfaces)}, nodes={len(self.nodes)})'
 
 
 class Node:
     def __init__(self, label, pos, vel):
         self.label, self.pos, self.vel = label, pos, vel
+
+    def __sub__(self, other):
+        return self.pos - other.pos
 
     def __repr__(self):
         return f'Node({self.label}, {self.pos}, {self.vel})'
@@ -64,12 +74,77 @@ class Node:
 class Surface:
     def __init__(self, label, nodes):
         self.label, self.nodes = label, nodes
+        self.vecs = []
+        self._construct_vectors()
+
+        # noinspection PyUnreachableCode
+        self.dir = np.cross(self.vecs[0], self.vecs[1])
+
+    def reverse_dir(self):
+        temp = self.nodes[1:]
+        self.nodes = [self.nodes[0]] + list(reversed(temp))
+        self.dir = -self.dir
+        self._construct_vectors()
+
+    def _construct_vectors(self):
+        # Convert nodes list to a NumPy array of positions
+        positions = np.array([node.pos for node in self.nodes])
+
+        # Shift the positions array by one position to the left to align each node with its next neighbor
+        shifted_positions = np.roll(positions, -1, axis=0)
+
+        # Subtract the original positions from the shifted positions to get the vectors
+        self.vecs = shifted_positions - positions
 
     def __eq__(self, other):
-        return sorted(self.nodes) == sorted(other.nodes)
+        return sorted([node.label for node in self.nodes]) == sorted([node.label for node in other.nodes])
+
+    def __getitem__(self, item):
+        return self.nodes[item]
+
+    def __repr__(self):
+        return f'Surface({self.label}, {[node.label for node in self.nodes]})'
 
 
 class Element:
-    def __init__(self, label, element_type, connectivity):
+    def __init__(self, label, element_type, connectivity, surfaces):
         self.label, self.type, self.connectivity = label, element_type, connectivity
-        self.surfaces = []
+        self.surfaces = surfaces
+
+        points = np.array([node.pos for node in connectivity])
+        self.centroid = np.sum(points, axis=0)/len(points)
+        self.outify_surfaces()
+
+    def outify_surfaces(self):
+        for surf in self.surfaces:
+            internal_vec = self.centroid - surf[0].pos
+            if np.dot(internal_vec, surf.dir) > 0:
+                surf.reverse_dir()
+
+    def __repr__(self):
+        return f'Element({self.label}, {self.type!r}, {[node.label for node in self.connectivity]}, {[surf.label for surf in self.surfaces]})'
+
+
+class GlobalMesh:
+    def __init__(self, *MeshBodies):
+        self.mesh_bodies = MeshBodies
+        self.elements = np.concatenate([mesh.elements for mesh in self.mesh_bodies])
+        self.surfaces = np.concatenate([mesh.surfaces for mesh in self.mesh_bodies])
+        self.nodes = np.concatenate([mesh.nodes for mesh in self.mesh_bodies])
+        self.surface_count = np.concatenate([mesh.surface_count for mesh in self.mesh_bodies])
+
+        e_start = len(self.mesh_bodies[0].elements)
+        s_start = len(self.mesh_bodies[0].surfaces)
+        n_start = len(self.mesh_bodies[0].nodes)
+
+        for i in range(e_start, len(self.elements)):
+            # noinspection PyUnresolvedReferences
+            self.elements[i].label = i
+
+        for i in range(s_start, len(self.surfaces)):
+            # noinspection PyUnresolvedReferences
+            self.surfaces[i].label = i
+
+        for i in range(n_start, len(self.nodes)):
+            # noinspection PyUnresolvedReferences
+            self.nodes[i].label = i
